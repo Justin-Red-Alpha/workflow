8d6184c13b64759b8a86a2cbf206cd4847a18899
Justin <justin.yip@redalphacyber.com>
2025-10-08 16:34:37 +0800

    changed to build and test

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ("Chicken Rice!")
+def add(a, b):
+    return a + b
+
+def test():
+    assert add(1,2) == 3, "test failed"
+    assert add(3,4) == 8, "test failed"
+    assert add(5,6) != 10, "test failed"
+
+if __name__ == "__main__":
+    print(add(2, 3))
