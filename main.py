f6780b6562cf71fb13e5235dc2521971b2ceb225
Justin <justin.yip@redalphacyber.com>
2025-10-08 16:43:57 +0800

    changed to build and test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def add(a, b):
 
 def test():
     assert add(1,2) == 3, "test failed"
-    assert add(3,4) == 8, "test failed"
+    assert add(3,4) == 7, "test failed"
     assert add(5,6) != 10, "test failed"
 
 if __name__ == "__main__":
